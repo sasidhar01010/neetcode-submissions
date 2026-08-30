4c02c1033bc6556238f9d3d47867054cd35eb672
Sasidhar Reddy Navuluri <135300854+sasidhar01010@users.noreply.github.com>
2026-08-30 23:35:38 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())return false;
+        vector<char>v(26,0);
+        for(int i=0;i<s.size();i++){
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(v[i]!=0)return false;
+        }
+        return true;
+        
+    }
+};
