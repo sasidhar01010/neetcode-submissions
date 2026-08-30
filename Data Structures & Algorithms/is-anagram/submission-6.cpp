0d9cbb3cb81cc836a081a7199a8109e4c47a8d24
Sasidhar Reddy Navuluri <135300854+sasidhar01010@users.noreply.github.com>
2026-08-30 23:31:46 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())return false;
+        unordered_map<char,int>m;
+        for(int i=0;i<s.size();i++){
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            m[t[i]]--;
+            if(m[t[i]]<0)return false;
+        }
+        return true;
+    }
+};
