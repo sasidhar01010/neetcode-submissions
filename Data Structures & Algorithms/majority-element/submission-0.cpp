eda91a67148eb82be1bb120d5ede82f07ef6c80c
Sasidhar Reddy Navuluri <135300854+sasidhar01010@users.noreply.github.com>
2026-09-23 13:39:35 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans=0,count=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                ans=nums[i];
+            }
+            if(nums[i]==ans){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return ans;
+    }
+};
